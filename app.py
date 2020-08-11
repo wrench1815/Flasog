4dad4aee865ee8dc4d431d40393c7c39eab4de36
wrench1815 <hardeepkumar1815@gmail.com>
2020-08-12 00:39:41 +0530

    add templates

--- app.py
+++ app.py
@@ -5,3 +5,38 @@
 #
 #  Main file for Flasog
 ##################################
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [{
+    'author': 'Hardeep Kumar',
+    'title': 'Blog Post 1',
+    'content': 'First Post Content',
+    'datePublished': '11 Aug, 2020'
+}, {
+    'author': 'Hardeep Kumar',
+    'title': 'Blog Post 2',
+    'content': 'Second Post Content',
+    'datePublished': '11 Aug, 2020'
+}]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.errorhandler(404)
+def NotFound(e):
+    return render_template('404.html', title='404 not Found')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
